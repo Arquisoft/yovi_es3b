46d22bb75061c5eff35ad546af4b1c1bac5e487b
David Lopez Araujo <uo299774@uniovi.es>
2026-04-10 21:47:24 +0200

    Added common setup file to all test, to try to fix test failure

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,8 +4,15 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: ['./src/__tests__/setup.ts'],
     coverage: {
       reporter: ['text', 'lcov'],
+    },
+    env: {
+      VITE_FIREBASE_API_KEY: 'AIzaSyDummyKeyForTesting',
+      VITE_FIREBASE_AUTH_DOMAIN: 'test-project.firebaseapp.com',
+      VITE_FIREBASE_PROJECT_ID: 'test-project',
+      VITE_FIREBASE_APP_ID: '1:123456789:web:abcdef123456',
     },
   },
 })
